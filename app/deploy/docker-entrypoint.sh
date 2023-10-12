3287c46de4df9a968fd874eba88b5a67d86238e5
Maurice Guikema <mguikema@gmail.com>
2023-10-12 15:08:13 +0200

    enable fixtures

--- app/deploy/docker-entrypoint.sh
+++ app/deploy/docker-entrypoint.sh
@@ -14,8 +14,8 @@ python manage.py migrate --noinput
 echo Collecting static files
 python manage.py collectstatic --no-input
 
-# echo Load initial data
-# python manage.py loaddata initial_data
+echo Load initial data
+python manage.py loaddata initial_data
 
 echo Create superuser
 python manage.py createsuperuser --noinput || true
