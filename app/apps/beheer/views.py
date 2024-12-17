429d7c81ef19d3eece9555c91dd0a865454645d3
Maurice Guikema <maurice@mgui.nl>
2024-12-17 14:16:59 +0100

    beheer env links removed

--- app/apps/beheer/views.py
+++ app/apps/beheer/views.py
@@ -1,6 +1,5 @@
 from apps.categories.forms import CategoryAanmakenForm, CategoryAanpassenForm
 from apps.categories.models import Category
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import redirect, render
@@ -40,40 +39,10 @@ def root(request):
 
 @permission_required("authorisatie.beheer_bekijken", raise_exception=True)
 def beheer(request):
-    env_suffixs = {
-        settings.PRODUCTIE: "",
-        settings.ACCEPTATIE: "-acc",
-        settings.TEST: "-test",
-    }
-    beheer_url = "/beheer/"
-    domain = "forzamor.nl"
-
-    env_urls = [
-        (
-            f"https://planr{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}",
-            "PlanR",
-        ),
-        (
-            f"https://fixer{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}",
-            "FixeR",
-        ),
-        # (f"https://onderwerpen{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}", "Onderwerpen"),
-        (
-            f"https://ontdbblr{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}",
-            "OntdbblR",
-        ),
-        (
-            f"https://taakr{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}",
-            "TaakR",
-        ),
-    ]
-
     return render(
         request,
         "beheer/beheer.html",
-        {
-            "env_urls": env_urls,
-        },
+        {},
     )
 
 
