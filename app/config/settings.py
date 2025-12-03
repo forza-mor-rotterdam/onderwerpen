466f6da6d184095610098c42aff6a28acc599081
Maurice Guikema <maurice@mgui.nl>
2025-12-03 12:04:53 +0100

    fix: debug toolbar

--- app/config/settings.py
+++ app/config/settings.py
@@ -188,9 +188,29 @@ def show_debug_toolbar(request):
     return DEBUG and os.getenv("SHOW_DEBUG_TOOLBAR") in TRUE_VALUES
 
 
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.history.HistoryPanel",
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.alerts.AlertsPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+]
+
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": show_debug_toolbar,
     "INSERT_BEFORE": "</head>",
+    "IS_RUNNING_TESTS": False,
+    "ENABLE_STACKTRACES": True,
+    "UPDATE_ON_FETCH": True,
 }
 
 # Django REST framework settings
